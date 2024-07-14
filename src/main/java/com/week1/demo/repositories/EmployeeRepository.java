ecdc050114e3b77c0ea0ad709ee0c3c0fe2adcb4
romi.indan@gmail.com <romi.indan@gmail.com>
2024-07-14 16:24:45 +0530

    set comment of repository

--- src/main/java/com/week1/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/week1/demo/repositories/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+// this model is user to sql query of pre-define as: create,select, and more...
 @Repository
 public interface EmployeeRepository  extends JpaRepository<EmployeeEntity,Long> {
 
